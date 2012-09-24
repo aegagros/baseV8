d9af653e8c40c9875c8c454237e73679682e5c87
aegagros <manolis_paterakis@yahoo.gr>
2012-09-25 00:06:20 +0300

    Various documentation additions

--- include/Common.h
+++ include/Common.h
@@ -29,42 +29,67 @@ THE SOFTWARE.
 #include <string>
 
 /*
-#define ASSERT_PARAM_COUNT_THROW(argsv, count)	if(argsv.Length() != count) {											\
-													v8::String::AsciiValue s(args.Callee()->GetName()->ToString());		\
-													return v8::ThrowException(FormatString(								\
-														"%s(): function arity mismatch, expecting %d arguments, received %d",\
-														*s, count, argsv.Length())); }									\
-*/
+  Define a macro for checking function arity. This is to be called inside
+  template function binders.
+ */
+#define ASSERT_PARAM_COUNT_THROW(argsv, count)  if(argsv.Length() != count) { \
+                          v8::String::AsciiValue s(                           \
+                              args.Callee()->GetName()->ToString()            \
+                          );                                                  \
+                          return v8::ThrowException(FormatString(             \
+                            "%s(): function arity mismatch, expecting %d arguments, received %d",\
+                            *s, count, argsv.Length())); }                    \
+/*
 #define ASSERT_PARAM_COUNT_THROW
+ */
 
 /**
  * @brief The primary namespace for all classes and functions
  */
 namespace base {
-	void ExtractExternal(void** data, v8::Handle<v8::External> extData) {
-		*data = v8::External::Unwrap(extData);
-	}
+  /**
+   * @brief Helper function to extract arbitrary native data wrapped inside a
+   * v8::External object instance.
+   *
+   * @param data    Pointer to a void* pointer to store the data.
+   * @param extData The handle to the v8::External instance.
+   */
+  void ExtractExternal(void** data, v8::Handle<v8::External> extData) {
+    *data = v8::External::Unwrap(extData);
+  }
 
-	void ExtractExternal(void** data, v8::Handle<v8::Value> value) {
-		v8::Handle<v8::External> extData = v8::Handle<v8::External>::Cast(value);
-		if(!extData.IsEmpty())
-			*data = v8::External::Unwrap(extData);
-		else
-			*data = 0;
-	}
+  /**
+   * @brief Helper function to extract arbitrary native data wrapped inside a
+   * v8::Value object instance.
+   *
+   * @param data  Pointer to a void* pointer to store the data.
+   * @param value The handle to the v8::External instance.
+   */
+  void ExtractExternal(void** data, v8::Handle<v8::Value> value) {
+    v8::Handle<v8::External> extData = v8::Handle<v8::External>::Cast(value);
+    if(!extData.IsEmpty())
+      *data = v8::External::Unwrap(extData);
+    else
+      *data = 0;
+  }
 
-	v8::Handle<v8::String> FormatString(const char* fmt, ...) {
-		static char buf[256] = "";
-		va_list args;
-		va_start(args, fmt);
-		vsprintf (buf, fmt, args);
-		va_end(args);
-		v8::HandleScope handle_scope;
-		v8::Handle<v8::String> str = v8::String::New(buf);
-		return handle_scope.Close(str);
-	}
+  /**
+   * @brief Helper function to construct a formatted v8::String object.
+   *
+   * @param fmt Format string according to stdlib printf() function.
+   */
+  v8::Handle<v8::String> FormatString(const char* fmt, ...) {
+    static char buf[256] = "";
+    va_list args;
+    va_start(args, fmt);
+    vsprintf (buf, fmt, args);
+    va_end(args);
+    v8::HandleScope handle_scope;
+    v8::Handle<v8::String> str = v8::String::New(buf);
+    return handle_scope.Close(str);
+  }
 
-	typedef unsigned char byte_t;
+  typedef unsigned char byte_t;
 }
 
 #endif /* COMMON_H_ */
