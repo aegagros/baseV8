30dfbc21ca69b023ed18b691f22c6b9935b41898
aegagros <manolis_paterakis@yahoo.gr>
2012-10-13 21:55:40 +0300

    Writing v8test.cpp as a generic test module handler

--- src/v8test.cpp
+++ src/v8test.cpp
@@ -1,24 +1,22 @@
-//============================================================================
-// Name        : v8test.cpp
-// Author      :
-// Version     :
-// Copyright   :
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
+// v8test.cpp
 #include <stdio.h>
 #include <v8/v8.h>
 #include <vector>
 #include "../include/baseV8.h"
-#include "test_decls.h.h"
+#include "TestModule.h"
 
-using namespace v8;
+void getModules(std::vector<TestModule*>& modules) {
+	// add modules to vector array
+}
 
-Handle<Value> printCallback(const Arguments& args) {
+/*
+	A function to print to the standard output
+ */
+v8::Handle<Value> printCallback(const v8::Arguments& args) {
 	if (args.Length() < 1) {
 	  return v8::Undefined();
 	}
-	HandleScope scope;
+	v8::HandleScope scope;
 	for (int i = 0; i < args.Length(); i++) {
 		String::Utf8Value value(args[i]);
 		printf("%s ", *value);
@@ -27,15 +25,21 @@ Handle<Value> printCallback(const Arguments& args) {
 	return v8::Undefined();
 }
 
+/*
+	An exception handler callback
+ */
 void handleExceptions(v8::TryCatch& handler) {
 	if (!handler.HasCaught()) {
 		return;
 	}
-	Handle<Value> exception = handler.Exception();
+	v8::Handle<Value> exception = handler.Exception();
 	String::AsciiValue exception_str(exception);
 	printf("ERROR: %s\n", *exception_str);
 }
 
+/*
+	A readfile helper function
+ */
 bool readfile(const char* filename, std::string& buffer) {
 	buffer = "";
 	FILE* file = fopen(filename, "r");
@@ -52,54 +56,79 @@ bool readfile(const char* filename, std::string& buffer) {
 	size_t res = fread(buf, 1, size, file);
 
 	if (res < size) {
-		printf("file corrupt");
+		// printf("file corrupt");
 		return false;
 	}
 
 	buf[size] = '\0';
 	fclose(file);
 
-	printf("read %d bytes", size);
+	// printf("read %d bytes", size);
 	buffer = buf;
 	return true;
 }
 
 
 int main(int argc, char* argv[]) {
-	Handle<Script> script;
+	v8::Handle<Script> script;
 	std::string buffer;
 
-	HandleScope handle_scope;
-	Handle<ObjectTemplate> global = ObjectTemplate::New();
-	/**
-	 * TODO: It's better to use the framework to bind the 'print' function. Find a
-	 * way to use a baseV8::ClassDef<> or a baseV8::TableDef to construct a global
-	 * object for use with a context.
-	 */
-	global->Set(String::New("print"), FunctionTemplate::New(printCallback));
+	// Initialize v8 engine
+	v8::HandleScope handle_scope;
 	Persistent<Context> context = Context::New(NULL, global);
 
-	Context::Scope context_scope(context);
+	base::TableDef global("global");
+	global.callback("print", printCallback);
 	TryCatch handler;
 
-  // TODO: no need
-	for (int i=1; i < argc; i++) {
-		printf("File '%s': reading...", argv[i]);
-		if(!readfile(argv[i], buffer))
-			continue;
-		Handle<String> source = String::New(buffer.c_str());
+	// Initialize test modules
+	std::vector<TestModule*> modules;
 
-		printf("...compiling...");
+	for (int i=0; i < modules.size(); i++) {
+		TestModule* module = modules[i];
+		// init module
+		printf("Initializing module '%s'...", module->getName());
+		if(!module->init()) {
+			printf("failed!\n");
+			continue;
+		}
+		else {
+			printf("%s\n", );("ok\n");
+		}
+		// read script file
+		printf("Reading file '%s'...", module->getScriptFilename());
+		if(!readfile(module->getScriptFilename(), buffer)) {
+			printf("failed!\n");
+			continue;
+		}
+		else {
+			printf("%s\n", );("ok\n");
+		}
+		// bind interface
 		handler.Reset();
+		module->bindInterface(global);
+		handleExceptions(handler);
+		// compile script
+		handler.Reset();
+		v8::Handle<String> source = v8::String::New(buffer.c_str());
 		script = Script::Compile(source);
 		handleExceptions(handler);
-
-		printf("...executing:\n");
+		// run script
 		handler.Reset();
 		Handle<Value> result = script->Run();
 		handleExceptions(handler);
+		// print srcipt output
 		String::AsciiValue ascii(result);
-		printf("result of script: %s\n", *ascii);
+		printf("result of script:\n-----------------\n%s\n", *ascii);
+		// shut down module
+		printf("Shutting down module '%s'...", module->getName());
+		if(!module->shutdown()) {
+			printf("failed!\n");
+			continue;
+		}
+		else {
+			printf("%s\n", );("ok\n");
+		}
 	}
 
 	context.Dispose();
