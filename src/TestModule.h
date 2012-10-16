4027e73ac8a986f6e1dabcfc70a651224b906300
aegagors <manolis_paterakis@yahoo.gr>
2012-10-17 00:11:30 +0300

    Minor changes to TestModule.h

--- src/TestModule.h
+++ src/TestModule.h
@@ -1,10 +1,16 @@
 #include "../include/baseV8.h"
+#include <string>
 
 class TestModule {
+private:
+  std::string m_name;
 public:
+  TestModule(const char* name) : m_name(name) {};
+  virtual ~TestModule() {};
+
   virtual bool init() = 0;
   virtual bool shutdown() = 0;
   virtual void bindInterface(base::Def& parent) = 0;
   virtual const char* getScriptFilename() = 0;
-  virtual const char* getName() = 0;
+  virtual const char* getName() { return m_name.c_str(); };
 };
